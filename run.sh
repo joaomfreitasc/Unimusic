bcc0de94061eb6a8ca757797a53721beb06caf1b
Gustavo F. Oliveira <gufonsec.oliveira@gmail.com>
2025-10-29 20:39:21 -0300

    Corretion on docker scripts

--- run.sh
+++ run.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-export AWS_ACCESS_KEY_ID="AWS_ACCESS_KEY_ID"
-export AWS_SECRET_ACCESS_KEY="AWS_SECRET_ACCESS_KEY"
-export DB_URL="DB_URL"
-export DB_USERNAME="DB_USERNAME"
-export DB_PASSWORD="DB_PASSWORD"
-export S3_BUCKET_NAME="S3_BUCKET_NAME"
+export AWS_ACCESS_KEY_ID=""
+export AWS_SECRET_ACCESS_KEY=""
+export DB_URL=""
+export DB_USERNAME=""
+export DB_PASSWORD=""
+export S3_BUCKET_NAME=""
 
 cd unimusic-server
 mvn clean install -DskipTests
